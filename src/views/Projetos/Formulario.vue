9f254431c492b9d2ab21a13d4452458e3a12da59
Vinicios Neves <vinicios_neves@hotmail.com>
2021-12-22 20:45:31 +0000

    video 2.4

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -19,7 +19,7 @@
 
 <script lang="ts">
 import { useStore } from "@/store";
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 
@@ -33,28 +33,26 @@ export default defineComponent({
       type: String,
     },
   },
-  // mounted() {
-  //   if (this.id) {
-  //     const projeto = this.store.state.projeto.projetos.find(
-  //       (proj) => proj.id == this.id
-  //     );
-  //     this.nomeDoProjeto = projeto?.nome || "";
-  //   }
-  // },
-  // data() {
-  //   return {
-  //     nomeDoProjeto: "",
-  //   };
-  // },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projeto.projetos.find(
+        (proj) => proj.id == this.id
+      );
+      this.nomeDoProjeto = projeto?.nome || "";
+    }
+  },
+  data() {
+    return {
+      nomeDoProjeto: "",
+    };
+  },
   methods: {
     salvar() {
       if (this.id) {
-        this.store
-          .dispatch(ALTERAR_PROJETO, {
-            id: this.id,
-            nome: this.nomeDoProjeto,
-          })
-          .then(() => this.lidarComSucesso());
+        this.store.dispatch(ALTERAR_PROJETO, {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        }).then(() => this.lidarComSucesso());
       } else {
         this.store
           .dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
@@ -71,23 +69,12 @@ export default defineComponent({
       this.$router.push("/projetos");
     },
   },
-  setup(props) {
+  setup() {
     const store = useStore();
     const { notificar } = useNotificador();
-
-    const nomeDoProjeto = ref("")
-
-    if (props.id) {
-      const projeto = store.state.projeto.projetos.find(
-        (proj) => proj.id == props.id
-      );
-      nomeDoProjeto.value = projeto?.nome || "";
-    }
-
     return {
       store,
       notificar,
-      nomeDoProjeto
     };
   },
 });
